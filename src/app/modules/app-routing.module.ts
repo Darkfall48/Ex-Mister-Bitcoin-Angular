7507d2a960f5a640b036ce1d8cae313e095e4ddb
Darkfall48 <sidney@sebban.be>
2023-02-20 22:45:55 +0200

    Frontend - Implemented: Routing & Navigation

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactDetailsComponent } from '../pages/contact-details/contact-details.component';
+import { ContactEditComponent } from '../pages/contact-edit/contact-edit.component';
+import { ContactIndexComponent } from '../pages/contact-index/contact-index.component';
+import { HomeComponent } from '../pages/home/home.component';
+import { StatsComponent } from '../pages/stats/stats.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'contact/edit/:_id',
+    component: ContactEditComponent,
+  },
+  {
+    path: 'contact/edit',
+    component: ContactEditComponent,
+  },
+  {
+    path: 'contact/:_id',
+    component: ContactDetailsComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactIndexComponent,
+  },
+  {
+    path: 'stats',
+    component: StatsComponent,
+  },
+  {
+    path: '',
+    component: HomeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -1,13 +1,23 @@
+//? Libraries
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//? Modules
 import { AppRoutingModule } from './app-routing.module';
+//? Components
 import { AppComponent } from '../app-root/app.component';
 import { HomeComponent } from '../pages/home/home.component';
+import { AppHeaderComponent } from '../cmps/app/app-header/app-header.component';
+import { AppFooterComponent } from '../cmps/app/app-footer/app-footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AppHeaderComponent,
+    AppFooterComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
 })
 export class AppModule {}
